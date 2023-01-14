51203542ff200acc803565f4a68a6146271a22ab
wikipuzzles <wikipuzzles@techie.com>
2023-01-14 14:04:43 +0200

    header links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def header_tab(current, name, page)
+    if (name == current)
+      return content_tag(:span, name, class: 'current_link')
+    end
+    span_tag = content_tag(:span, name, class: 'page_link')
+    link_to(span_tag, '/'+page, :class => 'page_link')
+  end
 end

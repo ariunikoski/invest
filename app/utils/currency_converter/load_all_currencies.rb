c7617cd6397b6bcd43de38c2f2de259233e165d1
wikipuzzles <wikipuzzles@techie.com>
2023-02-07 11:16:22 +0200

    log exchange rate update

--- app/utils/currency_converter/load_all_currencies.rb
+++ app/utils/currency_converter/load_all_currencies.rb
@@ -8,8 +8,11 @@ module CurrencyConverter
         cc = CurrencyConverter.new(rate.currency_code)
         er = cc.load
         if (er > 0)
+          Log.info("Got rate for #{rate.currency_code} - setting it to #{er}")
           rate.exchange_rate = er
           rate.save
+        else
+          Log.warn("Failed to get rate for #{rate.currency_code}")
         end
       end
     end

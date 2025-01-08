57747d6dcdff45fc99178589e8842c3ad9c07de0
root <root@unikoski-HP-Laptop-14-ck0xxx>
2025-01-08 14:42:52 +0200

    Filter by zero or non-zero holdings

--- app/javascript/pack/filters.js
+++ app/javascript/pack/filters.js
@@ -30,6 +30,9 @@ function apply_filter(flag_style='all') {
 		}
 	}
 	
+	const zero_holdings = getFlag('zero_holdings')
+	const non_zero_holdings = getFlag('non_zero_holdings')
+	
 	const really_good_price = getFlag('filter_really_good_price')
 	const good_price = getFlag('filter_good_price')
 	const big_investment = getFlag('filter_big_investment')
@@ -37,23 +40,36 @@ function apply_filter(flag_style='all') {
 	const div_overdue = getFlag('filter_div_overdue')
 	const no_div_last_year = getFlag('filter_no_div_last_year')
 	const comments = getFlag('filter_comments')
+	
 	const badges_cols = document.querySelectorAll("td[tag='column_badges']");
+	const holdings_cols = document.querySelectorAll("td[tag='holdings']");
 	for (let ii = 0; ii < badges_cols.length; ii++) {
 		const elem = badges_cols[ii]
 		const current_badges = convertBadgesString(elem.getAttribute('data_badges'))
-		console.log('>>> ', elem, current_badges)
+		
 		let displayThis = false
-		displayThis = displayThis || hasRelevantBadge(really_good_price, 'really_good_price', current_badges)
-		displayThis = displayThis || hasRelevantBadge(big_investment, 'big_investment', current_badges)
-		displayThis = displayThis || hasRelevantBadge(good_price, 'good_price', current_badges)
-		displayThis = displayThis || hasRelevantBadge(under_performer, 'under_performer', current_badges)
-		displayThis = displayThis || hasRelevantBadge(div_overdue, 'div_overdue', current_badges)
-		displayThis = displayThis || hasRelevantBadge(no_div_last_year, 'no_div_last_year', current_badges)
-		displayThis = displayThis || hasRelevantBadge(comments, 'comments', current_badges)
+		if (matches_holdings_filters(holdings_cols[ii].innerText.trim(), zero_holdings, non_zero_holdings)) {
+		  // i.e. dont check the badges if it already failed holdings
+		  displayThis = displayThis || hasRelevantBadge(really_good_price, 'really_good_price', current_badges)
+		  displayThis = displayThis || hasRelevantBadge(big_investment, 'big_investment', current_badges)
+		  displayThis = displayThis || hasRelevantBadge(good_price, 'good_price', current_badges)
+		  displayThis = displayThis || hasRelevantBadge(under_performer, 'under_performer', current_badges)
+		  displayThis = displayThis || hasRelevantBadge(div_overdue, 'div_overdue', current_badges)
+		  displayThis = displayThis || hasRelevantBadge(no_div_last_year, 'no_div_last_year', current_badges)
+		  displayThis = displayThis || hasRelevantBadge(comments, 'comments', current_badges)
+		}
 		if (!displayThis) {
 			const row = elem.closest('tr')
   			row.classList.add('hidden');
 		}
+	}
+}
+
+function matches_holdings_filters(holdings, zero_holdings, non_zero_holdings) {
+	if (holdings === "0") {
+		return zero_holdings
+	} else {
+		return non_zero_holdings
 	}
 }
 

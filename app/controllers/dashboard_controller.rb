b10385328a9c305de67d97843d3f1027ebca7b8a
Ari <unikoski+mintLinux@yahoo.com>
2026-03-01 19:51:30 +0200

    attempt at mobile - will check on priduction

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,11 +3,15 @@ class DashboardController < ApplicationController
     # Your logic here if needed
     
     # Render the index view
-    render 'index'
+    render 'index', locals: { is_mobile: is_mobile? }
   end
   
   def load_email_body
     mm = Yahoo::Email.new(Yahoo::Email.email_actions[:get_message_body], params[:id])
     render plain: mm.get_mail_body, status: :ok
+  end
+
+  def is_mobile?
+    request.user_agent =~ /Mobile|webOS/
   end
 end

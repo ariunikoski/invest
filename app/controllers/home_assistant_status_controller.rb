3f63354b5473717775b9fd7b06bf56b6cc99f994
Ari <unikoski+mintLinux@yahoo.com>
2026-08-10 13:29:53 +0300

    no require_authentication  for ha

--- app/controllers/home_assistant_status_controller.rb
+++ app/controllers/home_assistant_status_controller.rb
@@ -1,5 +1,5 @@
 class HomeAssistantStatusController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, :require_authentication
   before_action :authenticate_home_assistant, only: [:create]
 
   def authenticate_home_assistant
